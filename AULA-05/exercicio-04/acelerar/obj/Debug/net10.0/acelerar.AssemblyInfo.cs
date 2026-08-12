89ec81178ba8563207d99f6cb38eea8ff0992b83
JonathanGaldinoDaSilvagit config --global user.namegit config --global user.name Jonathan <jonathan.g.silva@edu.senai.br>
2026-08-12 08:40:33 -0300

    RPG terminado

--- AULA-05/exercicio-04/acelerar/obj/Debug/net10.0/acelerar.AssemblyInfo.cs
+++ AULA-05/exercicio-04/acelerar/obj/Debug/net10.0/acelerar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("acelerar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e45b131a6a958185c4b2985fcbd22907ff010420")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed2b7f728273b6ad4251634e7ffcc63452c8ceb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("acelerar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("acelerar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
